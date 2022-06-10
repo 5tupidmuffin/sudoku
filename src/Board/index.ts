2e60d8acee6e921ebf01e0fd1b77f9384e20b196
5tupidmuffin <rameshwark9500@gmail.com>
2022-06-10 21:42:38 +0530

    created Board API

---
+++ src/Board/index.ts
@@ -0,0 +1,51 @@
+export default class Board {
+  cells: Element[];
+  highlightedCellIndex: null | number;
+  constructor(cells: Element[]) {
+    this.cells = cells;
+    this.highlightedCellIndex = null;
+  }
+
+  fillBoard(values: number[]) {
+    for (let i = 0; i < 81; i++) {
+      if (values[i]) {
+        this.cells[i].textContent = (<unknown>values[i]) as string;
+        this.cells[i].classList.add("preFilledCell");
+      } else {
+        this.cells[i].textContent = "";
+        this.cells[i].classList.remove("preFilledCell");
+      }
+    }
+  }
+
+  isPrefilled(index: number) {
+    return this.cells[index].classList.contains("preFilledCell");
+  }
+
+  highlightCell(index: number) {
+    this.cells[index].classList.add("highlightedCell");
+    this.highlightedCellIndex = index;
+  }
+
+  markInvalid(index: number) {
+    this.cells[index].classList.add("invalid");
+  }
+
+  removeHighlight() {
+    if (this.highlightedCellIndex === null) return;
+    this.cells[this.highlightedCellIndex].classList.remove("highlightedCell");
+    this.cells[this.highlightedCellIndex].classList.remove("invalid");
+  }
+
+  updateValue(index: number, value: number) {
+    if (this.isPrefilled(index))
+      throw new Error(`tried to update prefilled cell: ${index}`);
+    this.cells[index].textContent = (<unknown>value) as string;
+  }
+
+  removeValue(index: number, value: number) {
+    if (this.isPrefilled(index))
+      throw new Error(`tried to update prefilled cell: ${index}`);
+    this.cells[index].textContent = "";
+  }
+}
